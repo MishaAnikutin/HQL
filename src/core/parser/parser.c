eec5dc785ec97f2036a18c44dab0041aad506906
mishaanikutin <billyknife@mail.ru>
2024-05-04 03:26:42 +0300

    update parser infrastructure

--- src/core/parser/parser.c
+++ src/core/parser/parser.c
@@ -7,21 +7,5 @@
 void
 parseRequest (const char* request)
 {
-    if (strncmp(request, "CREATE TABLE", 12) == 0)
-    {
-        table_t* table = parseCreateSTMT(request);
-        
-        if (table == NULL) 
-        {
-            fprintf(stderr, "Failed to parse CREATE TABLE statement\n");
-            return NULL;
-        }
 
-        fprintf(stdout, "CREATE TABLE(0)\n");
-    }
-    else 
-    {
-        fprintf(stderr, "Failed to parse statement\n");
-        return NULL;
-    }
 }
---
+++ src/core/parser/tokens/operators.h
@@ -0,0 +1,20 @@
+#ifndef OPERATORS_H
+#define OPERATORS_H
+
+
+enum Operators {
+    EQ  =   "=",
+    NE  =  "!=",
+    ADD =   "+",
+    SUB =   "-",
+    MUL =   "*",
+    DIV =   "/",
+    GT  =   ">",
+    LT  =   "<",
+    GE  =  ">=",
+    LE  =  "<=",
+    AND = "AND",
+    OR  =  "OR",
+};
+
+#endif
